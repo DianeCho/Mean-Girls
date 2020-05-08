430a753c2a12e30f91f69590cedcefb807d47fa4
DianeCho <diane.kim.cho@gmail.com>
2020-05-08 14:53:26 -0500

    CSS- update
    style H1 elements and div containers for homepage

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
-console.log("hello world");
+function quizPage() {
 
+    let x = document.getElementById("quizPage");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+quizPage(console.log("working1"));
+
+function gifPage() {
+
+    let x = document.getElementById("gifPage");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+gifPage(console.log("working2"));
+
+function resultsPage() {
+
+    let x = document.getElementById("resultsPage");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+resultsPage(console.log("working3"));
 
